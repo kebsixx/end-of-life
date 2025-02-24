fb1e2590699f1f3a5ee94f59d79947a2a56199f9
kebsixx <rijikanw@gmail.com>
2025-02-24 10:09:05 +0700

    add status in category

--- database/migrations/2025_02_18_123340_create_categories_table.php
+++ database/migrations/2025_02_18_123340_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('name');
-            $table->string('status');
+            $table->enum('status', ['aktif', 'non aktif'])->default('aktif');
             $table->string('description');
             $table->timestamps();
         });
