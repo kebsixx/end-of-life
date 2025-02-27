8fe7157bb37cec8d571057fb0e25d6551ee58aa5
kebsixx <rijikanw@gmail.com>
2025-02-27 15:29:03 +0700

    add ProductByManufactureChart widget to visualize products by manufacture

---
+++ app/Filament/Widgets/ProductByManufactureChart.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Manufactur;
+use Filament\Widgets\ChartWidget;
+
+class ProductByManufactureChart extends ChartWidget
+{
+    protected static ?string $heading = 'Products by Manufacture';
+
+    protected static ?int $sort = 2;
+
+    protected function getOptions(): array
+    {
+        return [
+            'indexAxis' => 'y',
+            'scales' => [
+                'x' => [
+                    'grid' => [
+                        'display' => false,
+                    ],
+                ],
+                'y' => [
+                    'grid' => [
+                        'display' => false,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function getData(): array
+    {
+        $manufactures = Manufactur::all();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Products by Manufacture',
+                    'data' => $manufactures->map(fn($manufacture) => 1)->toArray(),
+                    'backgroundColor' => '#3b82f6', // Blue color
+                    'borderColor' => '#2563eb',
+                    'borderWidth' => 1,
+                ],
+            ],
+            'labels' => $manufactures->pluck('name')->toArray(),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
