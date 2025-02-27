d9cf84297904f64846b8150693f7680bcd2ca61f
kebsixx <rijikanw@gmail.com>
2025-02-27 15:12:37 +0700

    add StatsOverview widget to display product and version statistics

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Category;
+use App\Models\Manufactur;
+use App\Models\Version;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Products', Category::count())
+                ->description('Total products in categories')
+                ->descriptionIcon('heroicon-m-square-3-stack-3d')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+
+            Stat::make('90 Days Notifications', Manufactur::where('notify_90_days', true)->count())
+                ->description('Products with 90 days notification')
+                ->descriptionIcon('heroicon-m-bell-alert')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('warning'),
+
+            Stat::make('Total Versions', Version::count())
+                ->description('Total version releases')
+                ->descriptionIcon('heroicon-m-code-bracket')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('info'),
+        ];
+    }
+}
