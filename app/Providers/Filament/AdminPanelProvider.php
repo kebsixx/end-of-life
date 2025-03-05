d4483066672f882930af2b5b74b12f4473851de7
kebsixx <rijikanw@gmail.com>
2025-03-05 14:36:34 +0700

    update sidebar behavior to be collapsible on desktop in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,7 +36,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogoHeight("40px")
             ->brandName('End Of Life')
             ->favicon('/images/favicon.png')
-            ->sidebarFullyCollapsibleOnDesktop()
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
