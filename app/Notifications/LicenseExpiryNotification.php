c444dcc91c8392460312d7262d8ca21c688ea122
kebsixx <rijikanw@gmail.com>
2025-03-05 21:04:10 +0700

    add last notification time

--- app/Notifications/LicenseExpiryNotification.php
+++ app/Notifications/LicenseExpiryNotification.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notification;
 use Filament\Notifications\Notification as FilamentNotification;
 use App\Models\Manufactur;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class LicenseExpiryNotification extends Notification
 {
@@ -16,13 +17,20 @@ class LicenseExpiryNotification extends Notification
 
     public function via(object $notifiable): array
     {
-        return []; // Kosongkan karena kita hanya menggunakan pop-up
+        return ['database']; // Store notifications in database
     }
 
     public function send(): void
     {
         $expiryDate = Carbon::parse($this->manufactur->last_installation_date);
         $now = now();
+
+        // Log notification attempt
+        Log::info('License expiry notification sent', [
+            'product' => $this->manufactur->name,
+            'expiry_date' => $expiryDate->format('Y-m-d H:i:s'),
+            'sent_at' => $now->format('Y-m-d H:i:s')
+        ]);
 
         FilamentNotification::make()
             ->title('License Expiration Warning')
